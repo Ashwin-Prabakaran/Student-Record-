cda47c81e3ffb78d108801c7bf0e511dd855b7f0
Ashwin P <84370118+Ashwin-Prabakaran@users.noreply.github.com>
2021-06-02 04:10:04 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StudentRecords.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+        <form id="form1" runat="server">  
+            <div align="center">  
+                <h1 style="color:green">Students Records</h1>  
+                <div style="width: 55%; background-image: url(Images/mks1.jpg); border: 5px solid yellow; border-radius: 25px; box-shadow: maroon 10px 10px 10px; color: black">  
+                    <table>  
+                        <tr>  
+                            <td></td>  
+                            <th>Name:  
+                            </th>  
+                            <th>Roll No :  
+                            </th>  
+                            <th>Age :  
+                            </th>  
+                            <th>Gender:  
+                            </th>  
+                        </tr>  
+                        <tr>  
+                            <td></td>  
+                            <td>  
+                                <asp:TextBox ID="Name" runat="server" Width="120px"></asp:TextBox>  
+                            </td>  
+                            <td>  
+                                <asp:TextBox ID="RollNo" runat="server" Width="120px"></asp:TextBox>  
+                            </td>  
+                            <td>  
+                                <asp:TextBox ID="Age" runat="server" Width="120px"></asp:TextBox>  
+                            </td>  
+                            <td>  
+                                <asp:TextBox ID="Gender" runat="server" Width="120px"></asp:TextBox>  
+                            </td>  
+                        </tr>  
+                        <tr>  
+  
+                            <td colspan="6" align="right">  
+                                <asp:Button ID="AddStudentDetails" runat="server" Style="color: White" Text="Add Student" OnClick="AddStudentDetails_Click" BackColor="#00248E" />  
+                            </td>  
+                        </tr>  
+                    </table>  
+  
+                    <div style="margin-left: 10px; margin-top: 10px">  
+                        <asp:GridView ID="GridView1" Width="700" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" OnRowCancelingEdit="GridView1_RowCancelingEdit" AutoGenerateColumns="False" runat="server" CellPadding="4" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px">  
+                            <Columns>  
+                                <asp:CommandField ShowEditButton="true" ShowDeleteButton="true" HeaderText="Operation" ItemStyle-Width="120px" />  
+  
+                                <asp:BoundField HeaderStyle-Width="120px" HeaderText="Name" DataField="Name">  
+                                    <HeaderStyle Width="120px"></HeaderStyle>  
+                                </asp:BoundField>  
+                                <asp:BoundField HeaderStyle-Width="120px" HeaderText="Roll No." DataField="Roll No">  
+                                    <HeaderStyle Width="120px"></HeaderStyle>  
+                                </asp:BoundField>  
+                                <asp:BoundField HeaderStyle-Width="120px" HeaderText="Age" DataField="Age">  
+                                    <HeaderStyle Width="120px"></HeaderStyle>  
+                                </asp:BoundField>  
+                                <asp:BoundField HeaderStyle-Width="120px" HeaderText="Gender" DataField="Gender">  
+                                    <HeaderStyle Width="120px"></HeaderStyle>  
+                                </asp:BoundField>  
+                            </Columns>  
+                            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />  
+                            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />  
+                            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" />  
+                            <RowStyle BackColor="White" ForeColor="#003399" />  
+                            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />  
+                            <SortedAscendingCellStyle BackColor="#EDF6F6" />  
+                            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />  
+                            <SortedDescendingCellStyle BackColor="#D6DFDF" />  
+                            <SortedDescendingHeaderStyle BackColor="#002876" />  
+                        </asp:GridView>  
+                    </div> 
+                    <div>
+                        Search:
+                        <asp:TextBox ID="txtSearch" runat="server" />
+                        <asp:Button Text="Search" runat="server" OnClick="Search" />
+                    </div>
+                    <br />  
+                </div>  
+            </div>  
+        </form>  
+    </body>  
+  
+</html>  
---
+++ Default.aspx.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace StudentRecords
+{
+	public partial class Default : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DefaultStudentRecord();
+            }
+        }
+        private void DefaultStudentRecord()
+        {
+            //creating dataTable   
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "StudentDetails";
+            dt.Columns.Add(new DataColumn("Name", typeof(string)));
+            dt.Columns.Add(new DataColumn("Roll No", typeof(string)));
+            dt.Columns.Add(new DataColumn("Age", typeof(string)));
+            dt.Columns.Add(new DataColumn("Gender", typeof(string)));
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+            //saving databale into viewstate   
+            ViewState["StudentDetails"] = dt;
+            //bind Gridview   
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        private void AddNewRecordRowToGrid()
+        {
+            // check view state is not null   
+            if (ViewState["StudentDetails"] != null)
+            {
+                //get datatable from view state   
+                DataTable dtCurrentTable = (DataTable)ViewState["StudentDetails"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //add each row into data table   
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["Name"] = Name.Text;
+                        drCurrentRow["Roll No"] = RollNo.Text;
+                        drCurrentRow["Age"] = Age.Text;
+                        drCurrentRow["Gender"] = Gender.Text;
+                    }
+                    //Remove initial blank row   
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+                    //add created Rows into dataTable   
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    //Bind Gridview with latest Row   
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+        protected void AddStudentDetails_Click(object sender, EventArgs e)
+        {
+            AddNewRecordRowToGrid();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Name", typeof(string)));
+            dt.Columns.Add(new DataColumn("Roll No", typeof(string)));
+            dt.Columns.Add(new DataColumn("Age", typeof(string)));
+            dt.Columns.Add(new DataColumn("Gender", typeof(string)));
+            string Name = string.Empty;
+            string RollNo = string.Empty;
+            string Age = string.Empty;
+            string Gender = string.Empty;
+            for (int i = 0; i < GridView1.Rows.Count; i++)
+            {
+                GridViewRow row = (GridViewRow)GridView1.Rows[i];
+                if (i != e.RowIndex)
+                {
+                    Name = row.Cells[1].Text;
+                    RollNo = row.Cells[2].Text;
+                    Age = row.Cells[3].Text;
+                    Gender = row.Cells[4].Text;
+                }
+                else
+                {
+                    Name = ((TextBox)row.Cells[1].Controls[0]).Text;
+                    RollNo = ((TextBox)row.Cells[2].Controls[0]).Text;
+                    Age = ((TextBox)row.Cells[3].Controls[0]).Text;
+                    Gender = ((TextBox)row.Cells[4].Controls[0]).Text;
+                }
+                DataRow dr = dt.NewRow();
+                dr["Name"] = Name;
+                dr["Roll No"] = RollNo;
+                dr["Age"] = Age;
+                dr["Gender"] = Gender;
+                dt.Rows.Add(dr);
+            }
+            GridView1.EditIndex = -1;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add(new DataColumn("Name", typeof(string)));
+                dt.Columns.Add(new DataColumn("Roll No", typeof(string)));
+                dt.Columns.Add(new DataColumn("Age", typeof(string)));
+                dt.Columns.Add(new DataColumn("Gender", typeof(string)));
+                string Name = string.Empty;
+                string RollNo = string.Empty;
+                string Age = string.Empty;
+                string Gender = string.Empty;
+                for (int i = 0; i < GridView1.Rows.Count; i++)
+                {
+                    GridViewRow row = (GridViewRow)GridView1.Rows[i];
+                    if (i != e.RowIndex)
+                    {
+                        Name = row.Cells[1].Text;
+                        RollNo = row.Cells[2].Text;
+                        Age = row.Cells[3].Text;
+                        Gender = row.Cells[4].Text;
+                        DataRow dr = dt.NewRow();
+                        dr["Name"] = Name;
+                        dr["Roll No"] = RollNo;
+                        dr["Age"] = Age;
+                        dr["Gender"] = Gender;
+                        dt.Rows.Add(dr);
+                    }
+                }
+                GridView1.EditIndex = -1;
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindStudentDetails();
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindStudentDetails();
+        }
+        private void BindStudentDetails()
+        {
+            if (ViewState["StudentDetails"] != null)
+            {
+                //get datatable from view state   
+                DataTable dtCurrentTable = (DataTable)ViewState["StudentDetails"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //add each row into data table   
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["Name"] = Name.Text;
+                        drCurrentRow["Roll No"] = RollNo.Text;
+                        drCurrentRow["Age"] = Age.Text;
+                        drCurrentRow["Gender"] = Gender.Text;
+                    }
+                    //Remove initial blank row   
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+                    //Bind Gridview with latest Row   
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+		protected void Search(object sender, EventArgs e)
+		{
+            try
+            {
+                DataTable dtCurrentTable = (DataTable)ViewState["StudentDetails"];
+                DataView dvCurrentTable = new DataView(dtCurrentTable);
+                dvCurrentTable.RowFilter = String.Format("Name like '%" + txtSearch.Text + "%' ");
+                GridView1.DataSource = dvCurrentTable;
+                GridView1.DataBind();
+                return;
+            }
+            catch (Exception ex)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + ex.Message + "');", true);
+            }
+        }
+	}
+}
